510ee766856a30054dd9ded1366f4326e24a399a
mikilaczy <118450248+mikilaczy@users.noreply.github.com>
2025-01-31 13:22:00 +0100

    01.21
    crudreact

---
+++ frontend/crudreact/frontend/kliens/src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './Home'
+import Regiok from './regiok.jsx'
+ 
+function App() {
+ 
+ return(
+  <div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/regiok" element={<Regiok/>}/>
+    </Routes>
+    </BrowserRouter>
+  </div>
+ )
+}
+ 
+export default App
